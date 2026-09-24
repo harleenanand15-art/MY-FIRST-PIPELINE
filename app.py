39d28b85293a2ae4def1b0645d938bc828dc51b9
harleenanand15-art <harleenanand15@gmail.com>
2026-09-24 17:36:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+
+@app.route('/')
+def home():
+    return "Hello! My CI/CD pipeline is working!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
